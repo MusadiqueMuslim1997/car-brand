9bfae182b532c46afaa6ec17e79ca9147ab8b606
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2024-02-14 22:08:51 +0500

    add

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
 
+var img_1 = document.getElementById("img-1");
+
+
+
+
+
+
+var car = {
+
+    mercidise:{
+        
+     mercidise_benz:{
+
+     
+
+
+     }
+
+
+     
+
+
+    }
+
+
+
+}
+
 
 
