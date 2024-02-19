3c2ce4e64734d13614cf5f8e39917b6a84a72203
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2024-02-19 22:21:07 +0500

    add

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
 var carlist = {
-   
-    BMW:{
 
-        BMWX5:{
-         
+    BMW: {
+
+        BMWX5: {
+
 
             name: "BMW X5",
             model: "2024",
             fulltype: "Hybrid",
             seat: "5 Seaters",
             speed: "130mph",
-            color:["red" , "white", "black"],
+            color: ["red", "white", "black"],
             price: "PKR 7.4 crore",
             image: "images/bmw5.png"
 
 
         },
 
-        BMWX7:{
+        BMWX7: {
             name: "BMW X7",
             model: "2024",
             fulltype: "Petrol",
             seat: "5 Seaters",
             speed: "155mph",
-            color:["red" , "white", "black"],
+            color: ["red", "white", "black"],
             price: "PKR 2.9 crore",
             image: "images/bm7.png"
 
         },
-        
-        BMWXM:{
+
+        BMWXM: {
             name: "BMW XM",
             model: "2023",
             fulltype: "Hybrid",
             seat: "5 Seaters",
             speed: "155mph",
-            color:["red" , "white", "black"],
+            color: ["red", "white", "black"],
             price: "PKR 3.4 crore",
             image: "images/bmm.png"
 
@@ -43,46 +43,46 @@ var carlist = {
 
     },
 
-    Honda:{
+    Honda: {
 
-        HondaCivic:{
+        HondaCivic: {
             name: "Honda Civic",
             model: "2022",
             fulltype: "Petrol",
             seat: "5 Seaters",
             speed: "137mph",
-            color:["red" , "white", "black"],
+            color: ["red", "white", "black"],
             price: "PKR 99 lacs",
             image: "images/hcivic.png"
         },
-        HondaCity:{
+        HondaCity: {
             name: "Honda City",
             model: "2023",
             fulltype: "Petrol",
             seat: "5 Seaters",
             speed: "195kmph",
-            color:["red" , "white", "black"],
+            color: ["red", "white", "black"],
             price: "PKR 47 lacs",
             image: "images/hcity.png"
         },
-        HondaAccord:{
+        HondaAccord: {
             name: "Honda Accord",
             model: "2023",
             fulltype: "Petrol",
             seat: "5 Seaters",
             speed: "116mph",
-            color:["red" , "white", "black"],
+            color: ["red", "white", "black"],
             price: "PKR 2.5 crore",
             image: "images/hacc.png"
         }
 
     },
 
-    Audi:{
+    Audi: {
 
     },
 
-    Suzuki:{
+    Suzuki: {
 
 
     }
@@ -95,20 +95,20 @@ var main = document.getElementById("main");
 var brand = document.getElementById("brand");
 var model = document.getElementById("model");
 brand.innerHTML += `<option>Select Brand Name</option>`
-for(var key in carlist){
-brand.innerHTML += `<option vlaue = "${key}">${key}</option>`
-for(var key1 in carlist[key]){
+for (var key in carlist) {
+    brand.innerHTML += `<option vlaue = "${key}">${key}</option>`
+    for (var key1 in carlist[key]) {
 
-    var obj = carlist[key][key1];
+        var obj = carlist[key][key1];
 
-    var colorDiv = "";
-    for (i = 0; i < obj.color.length; i++){
+        var colorDiv = "";
+        for (i = 0; i < obj.color.length; i++) {
 
-        colorDiv += `<div class = "${obj.color[i]}" ></div>`
-        
-    }
+            colorDiv += `<div class = "${obj.color[i]}" ></div>`
 
-    main.innerHTML += `
+        }
+
+        main.innerHTML += `
     <div class="card-1">
     <div class="card-img" id="img-1"><img src="${obj.image}""></div>
     <div class="card-info">${obj.name} (${obj.model})</div>
@@ -126,36 +126,38 @@ for(var key1 in carlist[key]){
 
 
 
+    }
 }
+
+
+for (var key in carDetail) {
+
 }
 
 
-
-
-
-function setModel(){
+function setModel() {
     model.innerHTML = "";
-    for (var key in carlist[brand.value]){
+    for (var key in carlist[brand.value]) {
         model.innerHTML += `<option vlaue = "${key}">${key}</option>`
     }
 }
-carDetail.style.display = "none";
-function searchCar(){
-    main.style.display = "none";
-    carDetail.style.display = "flex";
-    var carDetail = document.getElementById("carDetail")
-    var car = carlist[brand.value][model.value];
-    carDetail.innerHTML = `
-    <div class="c1">
-    <div class="card-img" id="img-1"><img src="${car.image}""></div>
-    <div class="card-info">${car.name} (${car.model})</div>
-    <div class="para">
-    ${car.fulltype}<br />
-   ${car.seat}<br />
-   ${car.speed}
-    </div>
-    <div class = "price"><h4>${obj.price}</h4></div>
-    <div class = "color">${colorDiv}<div>
+
+
+function searchCar() {
+ var carDetail = document.getElementById("car-detail");
+ var car = carlist[brand.value][model.value];
+
+ carDetail.innerHTML = `
+ <div class="c1">
+ <div class="card-img" id="img-1"><img src="${car.image}""></div>
+ <div class="card-info">${car.name} (${car.model})</div>
+ <div class="para">
+ ${car.fulltype}<br />
+ ${car.seat}<br />
+ ${car.speed}
+ </div>
+ <div class = "price"><h4>${car.price}</h4></div>
+ <div class = "color">${colorDiv}<div>
 </div>
-    `
+ `
 }
