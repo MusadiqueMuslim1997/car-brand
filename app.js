fe603af3966b0ee7fae90c02751393210736213a
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2024-02-19 19:32:45 +0500

    add

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var carlist = {
             name: "BMW X5",
             model: "2024",
             fulltype: "Hybrid",
+            seat: "5 Seaters",
             color:["red" , "white", "black"],
             price: "PKR 7.4 crore"
 
@@ -19,6 +20,7 @@ var carlist = {
             name: "BMW X7",
             model: "2024",
             fulltype: "Petrol",
+            seat: "5 Seaters",
             color:["red" , "white", "black"],
             price: "PKR 2.9 crore"
 
@@ -28,6 +30,7 @@ var carlist = {
             name: "BMW XM",
             model: "2023",
             fulltype: "Hybrid",
+            seat: "5 Seaters",
             color:["red" , "white", "black"],
             price: "PKR 3.4 crore"
 
@@ -59,11 +62,24 @@ for(var key1 in carlist[key]){
 
     var obj = carlist[key][key1];
 
+    var colorDiv = "";
+    for (i = 0; i < obj.color.length; i++){
+
+        colorDiv += `<div class = "${obj.color[i]}" ></div>`
+        
+    }
+
     main.innerHTML += `
     <div class="card-1">
     <div class="card-img" id="img-1"><img src="images/bmw5.png" alt=""></div>
-    <div class="card-info">${obj.name}</div>
-    <div class="para"></div>
+    <div class="card-info">${obj.name} (${obj.model})</div>
+    <div class="para">
+    ${obj.fulltype}<br />
+   ${obj.seat}
+    </div>
+    <div class = "price"><h4>${obj.price}</h4></div>
+    <div class = "color">${colorDiv}<div>
+
  </div>
     `
 
