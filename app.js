822c41f563b67f3da39b65874b195ae5c285e5dd
MusadiqueMuslim1997 <137433475+MusadiqueMuslim1997@users.noreply.github.com>
2024-02-19 18:15:22 +0500

    add

--- app.js
+++ app.js
@@ -1,31 +1,77 @@
+var carlist = {
+   
+    BMW:{
 
-var img_1 = document.getElementById("img-1");
+        BMWX5:{
+         
+            model: "2024",
+            fulltype: "Hybrid",
+            color:["red" , "white", "black"],
+            price: "PKR 7.4 crore"
 
 
 
+        },
 
+        BMWX7:{
+            model: "2024",
+            fulltype: "Petrol",
+            color:["red" , "white", "black"],
+            price: "PKR 2.9 crore"
 
-
-var car = {
-
-    mercidise:{
+        },
         
-     mercidise_benz:{
+        BMWXM:{
+            model: "2023",
+            fulltype: "Hybrid",
+            color:["red" , "white", "black"],
+            price: "PKR 3.4 crore"
 
-     
+        }
 
+    },
 
-     }
+    Honda:{
 
+    },
 
-     
+    Audi:{
+
+    },
+
+    Suzuki:{
 
 
     }
 
 
-
 }
 
 
+var main = document.getElementById("main");
 
+for (var key in carlist){
+ for (var key1 in carlist[key]){
+
+   var obj = carlist[kye][key1]
+
+   cards.innerHTML = ` <div class="card-1">
+   <div class="card-img" id="img-1"><img src="images/bmw5.png" alt=""></div>
+   <div class="card-info">${key1}</div>
+</div>
+
+<div class="card-1">
+   <div class="card-img" id="img-1"><img src="images/bmw5.png" alt=""></div>
+   <div class="card-info"></div>
+</div>
+
+<div class="card-1">
+   <div class="card-img" id="img-1"><img src="images/bmw5.png" alt=""></div>
+   <div class="card-info"></div>
+</div>`
+
+ }
+
+
+
+}
